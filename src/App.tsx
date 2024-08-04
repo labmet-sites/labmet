f23678bbd37f0a395c3c7a9eb44641605856ab4d
victor <gvictor567@gmail.com>
2024-08-04 02:20:58 -0300

    initial config

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import GlobalStyle from './styles/GlobalStyles';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Services from './pages/Services';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <Body>
+      <GlobalStyle/>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/servicos" element={<Services/>}/>
+        <Route path="/sobre" element={<About/>}/>
+        <Route path="/contato" element={<Contact/>}/>
+      </Routes>
+      </BrowserRouter>
+    </Body>
+  );
+}
+
+export default App;
+
+const Body = styled.div`
+  width: 100%;
+
+  * {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+  }
+`;
