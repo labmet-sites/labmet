eb7361d7544a6328d4104dafb7e967093a1e10e3
victor <gvictor567@gmail.com>
2024-08-08 08:48:07 -0300

    feat: areas section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,9 @@ import { Banner } from '../styles/styled/Banner';
 import { InfoSection } from '../styles/styled/InfoSection';
 import Header from '../components/Header/Header';
 import wave from '../assets/svg/wave.svg';
+import logo from '../assets/svg/logo.svg';
+import metrology from '../assets/imgs/metrology.png';
+import capacitacao from '../assets/imgs/capacitacao.jpg';
 import labmetName from '../assets/svg/labmet_name.svg';
 import { Link } from 'react-router-dom';
 
@@ -25,7 +28,34 @@ export default function Home() {
         <Link to="/sobre" className='about-link'>SAIBA MAIS</Link>
         <img className="banner-wave" src={wave} />
       </MainSection>
-      <InfoSection></InfoSection>
+      <InfoSection>
+        <p className='info-title'>ÁREAS DE ATUAÇÃO</p>
+        <ul className='areas'>
+          <div className='areas-bg'>
+          <img className='areas-logo' src={logo}/>
+          </div>
+          <li className='area'>
+            <img className='area-img' src={metrology}/>
+            <h4 className='title-1 area-title'>Metrologia Industrial</h4>
+            <h5 className='area-text'>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type 
+            specimen book
+            </h5>
+          </li>
+          <li className='area inverted-area'>
+          <img className='area-img' src={capacitacao}/>
+          <h4 className='title-2 area-title'>Consultoria e Capacitação</h4>
+          <h5 className='area-text'>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type 
+            specimen book
+            </h5>
+          </li>
+        </ul>
+      </InfoSection>
     </HomeStyles>
   );
 }
--- src/styles/styled/InfoSection.tsx
+++ src/styles/styled/InfoSection.tsx
@@ -3,6 +3,97 @@ import { theme } from '../theme';
 
 export const InfoSection = styled.section`
   width: 100%;
-  height: 600px;
+  height: 96vh;
   background-color: ${theme.colors.primary};
+  align-items: center;
+  flex-direction: column;
+
+  .info-title {
+    color: #fff;
+    font-size: 46px;
+    margin-top: 70px;
+    font-family: 'Kanit', sans-serif;
+    font-weight: 200;
+    letter-spacing: 3px;
+    margin-bottom: 30px;
+  }
+
+  .areas {
+    width: 100%;
+    height: 100%;
+    justify-content: space-between;
+    position: relative;
+
+    .areas-bg {
+      width: 100%;
+      height: 100%;
+      align-items: center;
+      justify-content: center;
+      position: absolute;
+      z-index: 1;
+      .areas-logo {
+        width: 50%;
+        opacity: 0.3;
+      }
+    }
+
+    .area {
+      width: max-content;
+      height: 100%;
+      flex-direction: column;
+      align-items: center;
+      position: relative;
+      flex-direction: column;
+      z-index: 99;
+      margin: 0px 80px;
+
+      .area-img {
+        width: 520px;
+        margin-bottom: 80px;
+      }
+
+      .area-title {
+        width: max-content;
+        height: max-content;
+        padding: 14px 36px;
+        background-color: #00b4fc;
+        color: #001449;
+        font-family: 'Kanit', sans-serif;
+        font-size: 26px;
+        font-weight: 300;
+        letter-spacing: 1px;
+        position: absolute;
+        z-index: 99;
+      }
+
+      .title-1 {
+        top: 270px;
+        left: 420px;
+      }
+
+      .area-text {
+        width: 520px;
+        color: #fff;
+        font-family: 'Kanit', sans-serif;
+        font-size: 24px;
+        font-weight: 200;
+        letter-spacing: 1px;
+        text-align: justify;
+      }
+    }
+
+    .inverted-area {
+      flex-direction: column-reverse;
+
+      .area-img {
+        margin-bottom: 90px;
+        margin-top: 100px;
+      }
+
+      .title-2 {
+        left: -300px;
+        bottom: 110px;
+      }
+    }
+  }
 `;
