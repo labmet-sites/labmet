c1e505a0f36f49693df22f91b73aaf6aa4a7c7ee
victor <gvictor567@gmail.com>
2024-10-11 18:01:38 -0300

    chore: files organization

---
+++ src/styles/theme.tsx
@@ -0,0 +1,10 @@
+export const theme = {
+    colors: {
+      primary: '#18294d',
+      secondary: '#5191c1',
+      third: '#111f3a',
+      contrast: '#fb6f24',
+      default: '#eaeaea'
+    },
+  };
+  
