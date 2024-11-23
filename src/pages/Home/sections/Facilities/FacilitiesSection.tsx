16f2f95f1e63260b8d6ac54c9a7239f5a9d5f992
victor <gvictor567@gmail.com>
2024-11-23 16:18:08 -0300

    feat: facilities carousel

--- src/pages/Home/sections/Facilities/FacilitiesSection.tsx
+++ src/pages/Home/sections/Facilities/FacilitiesSection.tsx
@@ -1,5 +1,50 @@
+import { useState } from 'react';
+import { Title } from '../../../../components/Title/Title';
+import { theme } from '../../../../styles/theme';
 import { FacilitiesStyled } from './FacilitiesStyled';
+import { TbArrowBadgeLeft } from 'react-icons/tb';
+import { TbArrowBadgeRight } from 'react-icons/tb';
 
 export default function FacilitiesSection() {
-  return <FacilitiesStyled></FacilitiesStyled>;
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+  const sections = ['1', '2', '3', '4'];
+
+  const handleNavigation = (direction: any) => {
+    if (direction === 'left') {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === 0 ? sections.length - 1 : prevIndex - 1,
+      );
+    } else if (direction === 'right') {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === sections.length - 1 ? 0 : prevIndex + 1,
+      );
+    }
+  };
+
+  return (
+    <FacilitiesStyled>
+      <Title $titleColor={theme.colors.primary}>Nossas Instalações </Title>
+      <div className="facilities-container">
+        {sections.map((item, index) => (
+          <div
+            key={index}
+            className={`facilitie sec-${index + 1}`}
+            style={{
+              transform: `translateX(${(index - currentIndex) * 100}%)`,
+              transition: 'transform 0.3s ease-in-out',
+            }}
+          >
+            {item}
+          </div>
+        ))}
+        <div className="arrow left" onClick={() => handleNavigation('left')}>
+          <TbArrowBadgeLeft />
+        </div>
+        <div className="arrow right" onClick={() => handleNavigation('right')}>
+          <TbArrowBadgeRight />
+        </div>
+      </div>
+    </FacilitiesStyled>
+  );
 }
--- src/pages/Home/sections/Facilities/FacilitiesStyled.tsx
+++ src/pages/Home/sections/Facilities/FacilitiesStyled.tsx
@@ -4,4 +4,85 @@ export const FacilitiesStyled = styled.section`
   width: 100%;
   height: 100vh;
   background-color: #f0a3df;
+  display: flex;
+  align-items: center;
+  padding-top: 50px;
+  flex-direction: column;
+
+  .facilities-container {
+    overflow: hidden;
+    position: relative;
+    display: flex;
+    width: 94%;
+    height: 80%;
+    margin-top: 70px;
+    .arrow {
+      position: absolute;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      z-index: 999;
+      width: 50px;
+      height: 100%;
+      transition: 300ms;
+      background-color: transparent;
+      overflow-x: hidden;
+
+      :nth-child(1) {
+        font-size: 44px;
+        color: #000;
+        transition: 300ms;
+      }
+
+      &:hover {
+        background-color: rgba(0, 0, 0, 0.2);
+        cursor: pointer;
+
+        :nth-child(1) {
+          color: #ffffff;
+          scale: 1.06;
+        }
+      }
+    }
+
+    .left {
+      left: 0px;
+      border-radius: 20px 0px 0px 20px;
+    }
+
+    .right {
+      right: 0px;
+      border-radius: 0px 20px 20px 0px;
+    }
+
+    .facilitie {
+      flex-shrink: 0;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background-color: #79b5e9;
+      color: white;
+      font-size: 30px;
+      border-radius: 20px;
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+
+    .sec-1 {
+      background-color: #bd9494;
+    }
+
+    .sec-2 {
+      background-color: #da8c58;
+    }
+    .sec-3 {
+      background-color: #c56cdb;
+    }
+    .sec-3 {
+      background-color: #ddeb5f;
+    }
+  }
 `;
