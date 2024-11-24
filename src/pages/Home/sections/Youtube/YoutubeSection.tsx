5012258fdd7c99062ecdcf3731e219f89c451f0b
victor <gvictor567@gmail.com>
2024-11-24 15:16:01 -0300

    style: under div yt section

--- src/pages/Home/sections/Youtube/YoutubeSection.tsx
+++ src/pages/Home/sections/Youtube/YoutubeSection.tsx
@@ -5,8 +5,10 @@ export default function YoutubeSection() {
   return (
     <YoutubeStyled>
       <img className="pci" src={pci}></img>
-      <div className="vid-1 videos">
-        {`
+      <div className="vid-1">
+        <div className='under-div'/>
+        <div className="vid-1-content">
+          {`
           Nosso Canal do YouTube apresenta atualmente três palestras que exploram temas essenciais no 
           densevolvimento da engenharia e suas tecnologias. Embora este seja o ponto de partida, nosso 
           objetivo é expandir continuamente essa biblioteca de conteúdos com novos materiais, para oferecer 
@@ -14,7 +16,8 @@ export default function YoutubeSection() {
           e trazem informações valiosas para quem deseja se aprofundar no campo. Acompanhe nosso canal e 
           fique atento às próximas atualizações!
         `}
-        <a href="yotube.com" className="card-1 yt-card"></a>
+          <a href="yotube.com" className="card-1"></a>
+        </div>
       </div>
       <div className="vid-2 videos">
         <a className="card-2 yt-card"></a>
--- src/pages/Home/sections/Youtube/YoutubeStyled.tsx
+++ src/pages/Home/sections/Youtube/YoutubeStyled.tsx
@@ -20,8 +20,50 @@ export const YoutubeStyled = styled.section`
   }
 
   .vid-1 {
-    background-color: #fff;
-    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    width: 90%;
+    height: 320px;
+    margin-bottom: 30px;
+    position: relative;
+    .under-div {
+      width: 100%;
+      height: 100%;
+      background-color: ${theme.colors.secondary};
+      position: absolute;
+      z-index: 1;
+      border-radius: 20px;
+      bottom: -50px;
+      right: -50px;
+    }
+    .vid-1-content {
+      width: 100%;
+      height: 100%;
+      background-color: #fff;
+      border-radius: 20px 40px 40px 20px;
+      position: relative;
+      padding: 16px 540px 16px 22px;
+      font-family: 'Montserrat', sans-serif;
+      font-weight: 400;
+      font-size: 23px;
+      line-height: 1.6;
+      text-align: justify;
+      z-index: 2;
+      box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+      
+      .card-1 {
+        width: 520px;
+        height: 100%;
+        position: absolute;
+        right: 0px;
+        top: 0px;
+        border-radius: 20px;
+        background-color: #5f5f5f;
+        border-radius: 0px 20px 20px 0px;
+        background-image: url('https://i.ytimg.com/vi/eEJIbPNLuFw/hqdefault.jpg?sqp=-oaymwEpCPYBEIoBSFryq4qpAxsIARUAAIhCGAHYAQHiAQwIGBACGAYgATgBQAE=&rs=AOn4CLBGHuhC43SxBp5P9bnt0CnKjdGQiQ');
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center center;
+      }
+    }
   }
 
   .vid-2 {
@@ -40,6 +82,7 @@ export const YoutubeStyled = styled.section`
     font-size: 23px;
     line-height: 1.6;
     text-align: justify;
+    z-index: 99;
 
     .yt-card {
       width: 520px;
@@ -49,14 +92,6 @@ export const YoutubeStyled = styled.section`
       top: 0px;
       border-radius: 20px;
       background-color: #5f5f5f;
-    }
-
-    .card-1 {
-      border-radius: 0px 20px 20px 0px;
-      background-image: url('https://i.ytimg.com/vi/eEJIbPNLuFw/hqdefault.jpg?sqp=-oaymwEpCPYBEIoBSFryq4qpAxsIARUAAIhCGAHYAQHiAQwIGBACGAYgATgBQAE=&rs=AOn4CLBGHuhC43SxBp5P9bnt0CnKjdGQiQ');
-      background-repeat: no-repeat;
-      background-size: cover;
-      background-position: center center;
     }
   }
 `;
