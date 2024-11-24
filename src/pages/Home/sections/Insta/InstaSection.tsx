533176f44a9c2a4d6e4cdb4c6d8eb95f105441b7
victor <gvictor567@gmail.com>
2024-11-24 18:16:22 -0300

    style: insta section

--- src/pages/Home/sections/Insta/InstaSection.tsx
+++ src/pages/Home/sections/Insta/InstaSection.tsx
@@ -1,5 +1,82 @@
 import { InstaStyled } from './InstaStyled';
+import logo from '../../../../assets/svg/logo.svg';
+import insta1 from '../../../../assets/img/insta1.jpg';
+import insta2 from '../../../../assets/img/insta2.jpg';
+import insta3 from '../../../../assets/img/insta3.jpg';
+import insta4 from '../../../../assets/img/insta4.jpeg';
+import { IoMdHeart } from 'react-icons/io';
+import { TbMessageCircleFilled } from 'react-icons/tb';
+import { BsFillSendFill } from 'react-icons/bs';
+import { MdBookmark } from 'react-icons/md';
+import { FaInstagram } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
 
 export default function InstaSection() {
-  return <InstaStyled>Insta</InstaStyled>;
+  const insta_imgs = [
+    {
+      img: insta1,
+      url: 'https://www.instagram.com/p/C9vDOLsvsQr/',
+    },
+    {
+      img: insta2,
+      url: 'https://www.instagram.com/p/C4O2kdaM3Nr/?img_index=1',
+    },
+    {
+      img: insta3,
+      url: 'https://www.instagram.com/p/DALl8FtRMCO/',
+    },
+    {
+      img: insta4,
+      url: 'https://www.instagram.com/p/DB1UCNYPF5O/',
+    },
+  ];
+
+  return (
+    <InstaStyled>
+      <div className="insta-posts">
+        {insta_imgs.map((i, index) => (
+          <a
+            className="insta-container"
+            key={index}
+            target="_blank"
+            href={i.url}
+          >
+            <div className="insta-header">
+              <div className="insta-user">
+                <img className="user-logo" src={logo} alt="Logo" />
+              </div>
+              <h6 className="user-name">labmetufcg</h6>
+            </div>
+            <img
+              className="insta-img"
+              src={i.img}
+              alt={`Instagram post ${i.img}`}
+            />
+            <div className="insta-footer">
+              <div className="left-icons">
+                <IoMdHeart className="icon-1 insta-icon" />
+                <TbMessageCircleFilled className="icon-2 insta-icon" />
+                <BsFillSendFill className="icon-3 insta-icon" />
+              </div>
+              <MdBookmark className="insta-icon" />
+            </div>
+          </a>
+        ))}
+      </div>
+      <p className="insta-text">
+        Nos acompanhe também em nossas redes sociais!
+      </p>
+      <div className="media-icons">
+        <a href="https://www.instagram.com/labmetufcg/" target="_blank">
+          <FaInstagram />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/labmet-laborat%C3%B3rio-de-metrologia-da-ufcg-90b942211/"
+          target="_blank"
+        >
+          <FaLinkedin />
+        </a>
+      </div>
+    </InstaStyled>
+  );
 }
--- src/pages/Home/sections/Insta/InstaStyled.tsx
+++ src/pages/Home/sections/Insta/InstaStyled.tsx
@@ -1,7 +1,117 @@
 import styled from 'styled-components';
+import { theme } from '../../../../styles/theme';
 
 export const InstaStyled = styled.section`
   width: 100%;
-  height: 100vh;
-  background-color: #75bdec;
+  height: 94vh;
+  background-color: ${theme.colors.primary};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 80px;
+
+  .insta-posts {
+    width: 100%;
+    height: max-content;
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-around;
+
+    .insta-container {
+      width: max-content;
+      height: max-content;
+      display: flex;
+      flex-direction: column;
+
+      .insta-header,
+      .insta-footer {
+        width: 100%;
+        height: 50px;
+        background-color: #fff;
+        display: flex;
+        align-items: center;
+        padding: 0px 18px 0px 18px;
+      }
+
+      .insta-header {
+        border-radius: 20px 20px 0px 0px;
+        justify-content: flex-start;
+
+        .insta-user {
+          height: 31px;
+          width: 31px;
+          clip-path: circle(50% at 50% 50%);
+          background-color: #e0e0e0;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          margin-right: 4px;
+
+          .user-logo {
+            width: 70%;
+          }
+        }
+
+        .user-name {
+          font-family: 'Montserrat', sans-serif;
+          font-size: 14px;
+          font-weight: 800;
+          color: #818181;
+        }
+      }
+
+      .insta-footer {
+        border-radius: 0px 0px 20px 20px;
+        justify-content: space-between;
+
+        .left-icons {
+          .icon-1 {
+            color: #ff0000;
+          }
+          .icon-3 {
+            font-size: 18px;
+          }
+
+          .insta-icon {
+            margin-right: 6px;
+          }
+        }
+        .insta-icon {
+          font-size: 22px;
+          color: #818181;
+        }
+      }
+
+      .insta-img {
+        width: 360px;
+        background-color: #eb6e6e;
+      }
+    }
+  }
+
+  .insta-text {
+    color: #fff;
+    margin-top: 40px;
+    font-family: 'Montserrat', sans-serif;
+    font-size: 34px;
+    font-weight: 700;
+    letter-spacing: 2px;
+  }
+
+  .media-icons {
+    margin-top: 22px;
+
+    a {
+      * {
+        color: #fff;
+        font-size: 48px;
+        margin: 0px 10px 0px 10px;
+        transition: 300ms;
+
+        &:hover {
+          scale: 1.08;
+        }
+      }
+    }
+  }
 `;
